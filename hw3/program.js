4f903117de6db322cc35e4eb27c2e8c19598f1b8
Ümit Büyükabalı <IS96467@gm.isbank.com.tr>
2016-05-16 15:37:54 +0300

    hw3 check-in

---
+++ hw3/program.js
@@ -0,0 +1,20 @@
+function createTriangle()
+{
+	for(var i = 1; i<= 100; i++)
+	{
+		if(i%3 == 0)
+		{
+			console.log("Fizz");
+		}
+		else if(i%5 == 0)
+		{
+			console.log("Buzz");
+		}
+		else
+		{
+			console.log(i);
+		}
+	}
+};
+
+createTriangle();
